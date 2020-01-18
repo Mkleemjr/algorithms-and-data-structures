db2cb39bffa79f134b7834c2975452625511d3f9
Andy Bettisworth <andy@accreu.com>
2020-01-18 02:29:53 -0600

    Write pseudocode

--- challenges/easy/DiagonalDifference/DiagonalDifference.go
+++ challenges/easy/DiagonalDifference/DiagonalDifference.go
@@ -1,5 +1,16 @@
 package DiagonalDifference
 
 func DiagonalDifference(arr [][]int) int {
+	// Initial first diagonal sum = 0
+	// Initial second diagonal sum = 0
+
+	// Loop over rows, i
+		// Loop over columns, j
+			// Check i == j
+				// Add sum first diagonal
+			// Check len(arr[0]) - 1 - i == j
+				// Add sum second diagonal
+
+	// Take absolute difference
 	return 0
 }
